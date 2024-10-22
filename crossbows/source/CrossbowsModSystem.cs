1d79d8b3f0e160e4fe0e9fd8202b4196631fda98
maltiez2 <maltiez2@gmail.com>
2024-10-22 05:42:29 +0300

    5.0

---
+++ crossbows/source/CrossbowsModSystem.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+namespace Crossbows;
+
+internal class CrossbowsModSystem : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterItemClass("Crossbows:Crossbow", typeof(CrossbowItem));
+        api.RegisterItemClass("Crossbows:MagazineCrossbow", typeof(MagazineCrossbowItem));
+    }
+}
